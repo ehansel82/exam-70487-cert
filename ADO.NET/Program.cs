6860e97cedbbb34de8113014e3a92feba9ecfe68
Eric Hansel <e.hansel.01@gmail.com>
2016-12-06 18:26:26 -0500

    initial commit

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUomWithDataAdapter();
+            Console.ReadLine();
+        }
+
+        private static void GetUomWithDataAdapter()
+        {
+            DataSet uomData = new DataSet("UomData");
+            DataTable uomTable = new DataTable("UnitMeasure");
+            uomData.Tables.Add(uomTable);
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["AdventureWorks"].ConnectionString))
+            {
+                var query = "select * from production.unitmeasure where unitmeasurecode like @mParam";
+                using (var sqlCommand = new SqlCommand(query, conn))
+                {
+                    sqlCommand.Parameters.AddWithValue("@mParam", $"m%");
+                    using (var adapter = new SqlDataAdapter(sqlCommand))
+                    {
+                        adapter.Fill(uomData, "UnitMeasure");
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total Records: {uomTable.Rows.Count}");
+        }
+    }
+}
