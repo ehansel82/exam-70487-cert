d397a5c6ac9cab4ecf9a8e01c308bd37a1de716c
Eric Hansel <e.hansel.01@gmail.com>
2016-12-28 21:00:56 -0500

    Start of WCF Data Service

---
+++ WCFDataService/testservice.svc.cs
@@ -0,0 +1,32 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace WCFDataService
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class testservice : DataService<AdventureWorksEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            // config.SetEntitySetAccessRule("MyEntityset", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+            config.SetEntitySetAccessRule("People", EntitySetRights.AllRead);
+            config.UseVerboseErrors = true;
+        }
+    }
+}
